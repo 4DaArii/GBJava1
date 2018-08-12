1abd539247fa548968274d095bdea645673744a7
Daria <nolightfor@mail.ru>
2018-08-12 18:20:37 +1000

    Выполнение задания 6: метод IsNegative

--- HW_1.java
+++ HW_1.java
@@ -9,7 +9,7 @@ class HW_1{
 	
 	public static void main(String[] args){
 		
-		//Задание 2. Объявление и инициализация переменных изученных типов данных
+		//Задание 2. Объявление и инициализация переменных изученных типов данных.
 		byte a = 8;
 		short b = 16;
 		int c = 32;
@@ -22,11 +22,12 @@ class HW_1{
 		System.out.println(calculate(3, 7, 54, 9));
 		System.out.println(SumInBorders(3, 4));
 		NegativeOrPositive(7);
+		System.out.println(IsNegative(0));
 	}
 	
-	/*Задание3 . Метод вычисляющий a * (b + (c / d))
+	/*Задание 3. Метод вычисляющий a * (b + (c / d))
 	 *и возвращающий результат, 
-	 *где a, b, c, d – входные параметры этого метода
+	 *где a, b, c, d – входные параметры этого метода.
 	 */
 	public static int calculate(int a, int b, int c, int d){
 		return a * (b + (c / d));
@@ -34,7 +35,7 @@ class HW_1{
 	
 	/*Задание 4. Метод, принимающий на вход два числа,
 	 *и проверяющий что их сумма лежит в пределах от 10 до 20(включительно),
-	 *если да – вернуть true, в противном случае – false
+	 *если да – вернуть true, в противном случае – false.
 	 */
 	 public static boolean SumInBorders(int num_1, int num_2){
 		int sum = num_1 + num_2;
@@ -54,6 +55,17 @@ class HW_1{
 			System.out.println(num + " - negative number");
 		} else{
 			System.out.println(num + " - positive number");
+		}
+	}
+	
+	/*Задание 6. Метод, которому в качестве параметра передается целое число,
+	 *метод должен вернуть true, если число отрицательное.
+	 */
+	public static boolean IsNegative(int num){
+		if (num < 0){
+			return true;
+		} else{
+			return false;
 		}
 	}
 }
