e97781b35d57abfd4c2b197daf95a3d3a5b57441
Daria <nolightfor@mail.ru>
2018-08-12 17:49:43 +1000

    Выполнение задания 3: метод calculate

--- HW_1.java
+++ HW_1.java
@@ -6,9 +6,10 @@
  */
  
 class HW_1{
+	
 	public static void main(String[] args){
 		
-		//Задание 1. Объявление и инициализация переменных изученных типов данных
+		//Задание 2. Объявление и инициализация переменных изученных типов данных
 		byte a = 8;
 		short b = 16;
 		int c = 32;
@@ -17,5 +18,13 @@ class HW_1{
 		double f = 15.72775;
 		char g = 'g';
 		boolean i = true;
+		
+		System.out.println(calculate(3, 7, 54, 9));
 	}
+	
+	//Задание3. Метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,  где a, b, c, d – входные параметры этого метода
+	public static int calculate(int a, int b, int c, int d){
+		return a * (b + (c / d));
+	}
+	
 }
