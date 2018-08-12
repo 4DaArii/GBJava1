9241182fc64f582f8ec6b648acef62a3de425ce7
Daria <nolightfor@mail.ru>
2018-08-12 17:39:21 +1000

    Выполнение задания 2: объявление и инициализация переменных

--- HW_1.java
+++ HW_1.java
@@ -7,6 +7,15 @@
  
 class HW_1{
 	public static void main(String[] args){
-	
+		
+		//Задание 1. Объявление и инициализация переменных изученных типов данных
+		byte a = 8;
+		short b = 16;
+		int c = 32;
+		long d = 64;
+		float e = 120.0f;
+		double f = 15.72775;
+		char g = 'g';
+		boolean i = true;
 	}
 }
