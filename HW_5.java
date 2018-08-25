61e2111ee5241c2a2071ccaf0ef569864412bb4d
Daria <nolightfor@mail.ru>
2018-08-25 18:55:08 +1000

    Started

--- HW_5.java
+++ HW_5.java
@@ -1,2 +1,12 @@
+/**
+ * Java 1. Lesson 5. Homework
+ *
+ * @author Daria Vdovenko
+ * @version dated Aug 25, 2018
+ */
+
 class HW_5 {
+    public static void main(String[] args) {
+        
+    }
 }
